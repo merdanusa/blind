0443f234bcbf9c05ec6a7649b7ff3da352c8e4e3
Merdan <wisew.tube@gmail.com>
2026-02-05 15:10:27 +0500

    init commit

--- infra/configs/db.config.js
+++ infra/configs/db.config.js
@@ -1,5 +1,19 @@
+import { configDotenv } from "dotenv";
+configDotenv();
 import mongoose from "mongoose";
 
 const MONGO_URI = process.env.MONGO_URI;
 
-export default async function connectDb() {}
+export default async function connectDb() {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("MongoDB connected!");
+  } catch (error) {
+    console.log("Database connection error:", error);
+    process.exit(1);
+  }
+}
+
+mongoose.connection.on("disconnected", () => {
+  console.log("MongoDB disconnected!");
+});
--- server.js
+++ server.js
@@ -2,12 +2,15 @@ import { configDotenv } from "dotenv";
 configDotenv();
 import express from "express";
 
-import authRoutes from "./modules/routes/authRoutes";
+import authRoutes from "./modules/routes/authRoutes.js";
+import connectDb from "./infra/configs/db.config.js";
 
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
 app.use("/api/auth", authRoutes);
+
+connectDb();
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}!`));
